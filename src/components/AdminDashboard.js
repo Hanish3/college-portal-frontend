6b68e439660de71d28de20d79e7f73eaa4947c0e
Lucifer <hanishtompla@gmail.com>
2025-11-03 14:15:01 +0530

    fix

---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <h1>Welcome, Admin/Faculty!</h1>
+            <p>This is the admin dashboard. You can search for students here.</p>
+        </div>
+    );
+};
+
+export default AdminDashboard;
